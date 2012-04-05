9b7dc923131b533bdc9758f43cac041ad90f8dae
Byron Katz <byronka@msn.com>
2012-04-05 13:43:56 +0000

    tagging the build 04041201
    git-svn-id: file:///home/byron/Desktop/svn/archival/REAP_older@2 7b726b34-247f-e111-ba7b-485b39788f86

---
+++ Tags/04041201/src/com/byronkatz/reap/general/HelpButtonOnClickWrapper.java
@@ -0,0 +1,21 @@
+package com.byronkatz.reap.general;
+
+import android.view.View;
+import android.view.View.OnClickListener;
+
+public class HelpButtonOnClickWrapper implements OnClickListener {
+
+  private ValueEnum ve;
+  
+  public HelpButtonOnClickWrapper(ValueEnum ve) {
+    this.ve = ve;
+  }
+  
+  @Override
+  public void onClick(View v) {
+    Utility.showHelpDialog(
+        ve.getHelpText(), ve.getTitleText(), v.getContext());
+  }
+
+
+}
